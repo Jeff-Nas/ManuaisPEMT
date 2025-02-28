2735251522eeb59c8dbe3c813ffd0821bb306f84
Jeferson Nascimento <jeferson.ifce@gmail.com>
2025-02-28 20:42:25 -0300

    Pequenas mudanças de estilo, criação dos objetos para tesouras jlg

---
+++ paginas/jlg/tesoura-eletrica/dataScript.js
@@ -0,0 +1,10 @@
+const manualData = {
+    "10RS":  [ 
+    { text: "6RS - 10RS - Serviço", link: "manuais/jlg/tesoura-eletrica/6RS-10RS/3123331-Serviços.pdf"},
+    { text: "6RS - 10RS - Peças", link: "manuais/jlg/tesoura-eletrica/6RS-10RS/6RS-10RS-peças.pdf"},
+    { text: "6RS - 10RS - Operação", link: "manuais/jlg/tesoura-eletrica/6RS-10RS/6RS-10RS-operações.pdf"}
+    ],
+
+    
+
+}
