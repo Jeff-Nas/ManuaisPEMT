b5e2966f32f27ba60f7415d5c8fc493416824c37
Jeferson Nascimento <jeferson.ifce@gmail.com>
2025-03-16 11:05:42 -0300

    ocultando elemnto swiper ao clicar no menu

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,26 +1,37 @@
 
 //DESTINADO AO SWIPPER
 const swiper = new Swiper('.slider-wrapper', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 5,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true,
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 5,
 
-    slidesPerView: 3,
-    //responsividade a definir: ver documentação https://swiperjs.com/swiper-api#initialize-swiper
-    breakpoints: {
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
 
-    }
-  });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  slidesPerView: 3,
+  //responsividade a definir: ver documentação https://swiperjs.com/swiper-api#initialize-swiper
+  breakpoints: {
+
+  }
+});
+
+//OCULTAR SWIPPER
+
+document.addEventListener('DOMContentLoaded', function () {
+  const menuButton = document.querySelector('.navbar-toggler')
+  const swiperContainer = document.querySelector('.slider-wrapper')
+
+  menuButton.addEventListener('click', function () {
+    swiperContainer.classList.toggle('active')
+  })
+})
