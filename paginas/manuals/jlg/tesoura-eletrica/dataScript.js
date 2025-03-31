4d030f6a4592d5548d28f6f5b0c4582c7dd8beac
Jeferson Nascimento <jeferson.ifce@gmail.com>
2025-03-30 21:25:41 -0300

    criação da página tesoura díesel - inserção de arequivos

--- paginas/manuals/jlg/tesoura-eletrica/dataScript.js
+++ paginas/manuals/jlg/tesoura-eletrica/dataScript.js
@@ -1,5 +1,5 @@
 const manualData = {
-    "10RS": [
+    "6RS - 10RS": [
         { text: "6RS - 10RS - Serviço", link: "/manuais/jlg/tesoura-eletrica/6RS-10RS/3123331-Serviços.pdf" },
         { text: "6RS - 10RS - Peças", link: "/manuais/jlg/tesoura-eletrica/6RS-10RS/6RS-10RS-peças.pdf" },
         { text: "6RS - 10RS - Operação", link: "/manuais/jlg/tesoura-eletrica/6RS-10RS/6RS-10RS-operações.pdf" }
@@ -13,6 +13,11 @@ const manualData = {
         { text: "1930ES -> 3246ES - Peças – PN 3121683 - 10-08-20 - NO. SÉRIE 0200239382 A 0200266434 E B200020297 AO ATUAL", link: "/manuais/jlg/tesoura-eletrica/1930ES-A-3246ES/2646ES,3246ES JLG - PEÇAS - PN 3121683 - 10-08-20 - NO. SÉRIE 0200239382 A 0200266434 E B200020297 AO ATUAL.pdf" },
         { text: "1930ES -> 3246ES - Operação - Global", link: "/manuais/jlg/tesoura-eletrica/1930ES-A-3246ES/3122377_05-01-13_Global---Operações.pdf" },
         { text: "1930ES -> 3246ES - Operação", link: "/manuais/jlg/tesoura-eletrica/1930ES-A-3246ES/3123697_I_ES-Scissors_JLG--Operações.pdf" }
+    ],
+
+    "3369LE-M4069LE": [
+        { text: "Parts - JLG 3369LE-4069LE service-manual", link: "/manuais/jlg/tesoura-eletrica/M4069LE/3369LE 4069LE M3369 M4069 - PEÇAS - PN 3121123 - 08-31-2016 - ANTERIOR 0200230092.pdf" },
+        { text: "Service - 3369LE 4069LE M3369 M4069 - PEÇAS - PN 3121123 - 08-31-2016 - ANTERIOR 0200230092", link: "/manuais/jlg/tesoura-eletrica/M4069LE/JLG 3369LE 4069LE service manual.pdf" }
     ]
 
 }
